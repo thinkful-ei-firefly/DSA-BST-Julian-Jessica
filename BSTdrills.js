5f9cd5d325895e42f5d9b266edf875113826222e
Jess Doyle <jessica.e.doyle@gmail.com>
2019-10-08 15:33:48 -0400

    P9

--- BSTdrills.js
+++ BSTdrills.js
@@ -1,6 +1,6 @@
 'use strict';
 const BinarySearchTree = require('./BinaryTree');
-const { findHeight, isBST } = require ('./BSThelperFns');
+const { findHeight, isBST, thirdLargest, balancedBST, duplicateTrees } = require ('./BSThelperFns');
 
 function main () {
   const BST = new BinarySearchTree();
@@ -12,12 +12,10 @@ function main () {
   BST.insert(2);
   BST.insert(5);
   BST.insert(7);
-  BST.left.key = 4;
-  console.log(BST);
-  console.log(BST.left);
-  console.log(isBST(BST));
+  
+  console.log(balancedBST(BST));
 }
-main();
+// main();
 
 function main2 () {
   const BST = new BinarySearchTree();
@@ -33,7 +31,7 @@ function main2 () {
   BST.insert('I');
   BST.insert('O');
   BST.insert('N');
-  findHeight(BST);
+  console.log(balancedBST(BST));
 }
 // main2();
 
@@ -47,4 +45,41 @@ function tree (t) {
 //This function returns the sum of all values within the tree. It will have runtime O(n),
 //since it will be called once on every node within the tree.
 
+function main3 () {
+  const BST = new BinarySearchTree();
 
+  BST.insert(4);
+  BST.insert(2);
+  BST.insert(6);
+  BST.insert(1);
+  BST.insert(3);
+  BST.insert(5);
+  BST.insert(7);
+
+  console.log(balancedBST(BST));
+}
+// main3();
+
+function main4 () {
+  const arr1 = [3, 5, 4, 6, 1, 0, 2];
+  const arr2 = [3, 1, 5, 2, 4, 6, 0];
+
+  const arr3 = [3, 4, 5, 6, 1, 0, 2];
+  const arr4 = [3, 1, 5, 2, 4, 6, 0];
+
+  const arr5 = [3, 4, 5, 6, 2];
+  const arr6 = [3, 1, 5, 2, 4, 6, 0];
+
+  const arr7 = [352, 4, 42, 6, 1, 3, 2];
+  const arr8 = [3, 1, 89, 587, 7, 4, 0];
+
+  const arr9 = [3, 5, 4, 6, 1, 0, 2];
+  const arr0 = [3, 5, 4, 6, 1, 0, 2];
+
+  console.log(duplicateTrees(arr1, arr2));
+  console.log(duplicateTrees(arr3, arr4));
+  console.log(duplicateTrees(arr5, arr6));
+  console.log(duplicateTrees(arr7, arr8));
+  console.log(duplicateTrees(arr9, arr0));
+}
+main4();
--- BSThelperFns.js
+++ BSThelperFns.js
@@ -29,24 +29,142 @@ function isBST (BST) {
   //if at any point, left > key || right < key, return false
   //if ok for now, perform check on subtrees left and right of key
   //if never false, return true
-  return checkNode(BST) === undefined;
+  return _checkNode(BST) === undefined;
   
 
 }
 
-function checkNode (node) {
+function _checkNode (node) {
   if (node.left) {
     if (node.left.key > node.key) {
       return false;
     }
-    checkNode(node.left);
+    _checkNode(node.left);
   }
   if (node.right) {
     if (node.right.key < node.key) {
       return false;
     }
-    checkNode(node.right);
+    _checkNode(node.right);
   }
 }
 
-module.exports = { findHeight, isBST };
+function thirdLargest (tree) {
+  //We have no guarantee of the structure of the tree, so we'll have to iterate recursively
+  //Through the entire tree to perform a comparison.
+  //We also need to track the three highest values in the tree at every step
+  //Use an array? [key1, key2, key3]
+  //Keep this array sorted so key1 < key2 <key3.
+  //For every node, perform the comparisons:
+  //if node.key > key1 && node.key < key2, key1 = node.key
+  //else if node.key > key2 && node.key < key3, key1 = key2, key2 = node.key
+  //else if node.key > key3, key1 = key2, key2 = key3, key3 = node.key
+  //then, if node.left, run thirdLargest on node.left, passing down modified array
+  //if node.right, run thirdLargest on node.right, passing down modified array
+  //at end, return key1
+  let top3arr = [tree.key, tree.key, tree.key];
+  _checkValue(tree, top3arr);
+  return top3arr[0];
+}
+
+function _checkValue (node, arr) {
+  //check the value of the node and modify the value of the array appropriately
+  if (node.key > arr[0] && node.key < arr[1]) {
+    arr[0] = node.key;
+  }
+  else if (node.key > arr[1] && node.key < arr[2]) {
+    arr[0] = arr[1];
+    arr[1] = node.key;
+  }
+  else if (node.key > arr[2]) {
+    arr[0] = arr[1];
+    arr[1] = arr[2];
+    arr[2] = node.key;
+  }
+  //if there are further nodes to the left or right, perform the same
+  // calculation on the lower branches
+  if (node.left) {
+    _checkValue(node.left, arr);
+  }
+  if (node.right) {
+    _checkValue(node.right, arr);
+  }
+}
+
+function _leafDist2 (t, count = 0, arr = []) {
+  //increase count by 1 if node exists
+  //if the node has no children, push count to arr
+  //if the node has a left child, recursively call the left child
+  //if the node has a right child, recursively call the right child
+  if (t) count++;
+  if (!t.left && !t.right) {
+    arr.push(count);
+  }
+  if (t.left) {
+    _leafDist2(t.left, count, arr);
+  }
+  if (t.right) {
+    _leafDist2(t.right, count, arr);
+  }
+}
+
+function balancedBST (tree) {
+  const branchLengths = [];
+  _leafDist2(tree, 0, branchLengths);
+  const longest = Math.max(...branchLengths);
+  const shortest = Math.min(...branchLengths);
+  return (longest - shortest) <= 1;
+}
+
+function duplicateTrees (arr1, arr2) {
+  //given 2 arrays, see if nodes would be formed the same if array elements
+  //were inserted into a binary search tree in index order
+  //for each node, take the value of the node and the 2 arrays of values that
+  //would be added to it
+  //find the first item lower than the node value in each array
+  //find the first item higher than the node value in each array
+  //first higher should be equal between arrays
+  //first lower should be equal between arrays
+  //if not, return false
+  //if ok, filter remaining values into those that are larger or smaller than the start node
+  //if elements still remin in filtered array, recursively call
+  //with higher & lower values and arrays
+  const firstVal = arr1[0];
+  if (firstVal !== arr2[0]) {
+    return false;
+  }
+  arr1 = arr1.filter(val => val !== firstVal);
+  arr2 = arr2.filter(val => val !== firstVal);
+
+  return _checkNodeBranching(firstVal, arr1, arr2) === undefined;
+}
+
+function _checkNodeBranching(key, arr1, arr2) {
+  const firstlower1 = arr1.find(val => val < key);
+  const firstlower2 = arr2.find(val => val < key);
+  const firsthigher1 = arr1.find(val => val > key);
+  const firsthigher2 = arr2.find(val => val > key);
+  if (firstlower1 !== firstlower2 || firsthigher1 !== firsthigher2) {
+    return false;
+  }
+  //filter arr1 and arr2 into values higher and lower than key, excluding higher and lower1/2
+  const arr1lower = arr1.filter(val => val < key && val !== firstlower1);
+  const arr2lower = arr2.filter(val => val < key && val !== firstlower2);
+  const arr1higher = arr1.filter(val => val > key && val !== firsthigher1);
+  const arr2higher = arr2.filter(val => val > key && val !== firsthigher2);
+
+  if (arr1lower.length !== arr2lower.length || arr1higher.length !== arr2higher.length) {
+    return false;
+  }
+  
+  if (arr1lower.length > 0) {
+    _checkNodeBranching(firstlower1, arr1lower, arr2lower);
+  }
+
+  if (arr1higher.length > 0) {
+    _checkNodeBranching(firsthigher1, arr1higher, arr2higher);
+  }
+
+}
+
+module.exports = { findHeight, isBST, thirdLargest, balancedBST, duplicateTrees };
